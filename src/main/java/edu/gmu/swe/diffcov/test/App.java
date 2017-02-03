5dd8c5ae1763f5ef02b2e9df9c5491a273f1a07c
Jonathan Bell <jbell@cs.columbia.edu>
2017-02-03 17:20:30 -0500

    initial commit

---
+++ src/main/java/edu/gmu/swe/diffcov/test/App.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe.diffcov.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
