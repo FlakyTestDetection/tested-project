b919babb031ec4895e35c88ce846e2539656d60b
Jonathan Bell <jbell@cs.columbia.edu>
2017-02-04 17:38:06 -0500

    Add new test

---
+++ src/test/java/edu/gmu/swe/diffcov/test/CopyOfAppTest.java
@@ -0,0 +1,14 @@
+package edu.gmu.swe.diffcov.test;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CopyOfAppTest {
+
+	@Test
+	public void testApp2() {
+		new App().bar();
+	}
+}
