38fc4de30d32e73afc1ff856fd2bca8912779f12
Jonathan Bell <jbell@cs.columbia.edu>
2017-02-05 11:23:24 -0500

    non-strutural changes

--- src/main/java/edu/gmu/swe/diffcov/test/App.java
+++ src/main/java/edu/gmu/swe/diffcov/test/App.java
@@ -8,16 +8,20 @@ public class App
 {
 	public void otherMethod()
 	{
-		
+		System.out.println("This change will be missed");
 	}
     public void foo()
     {
     	bar();
+    	if(i == 0)
+    		System.out.println("i==0");
     }
     
     public void bar()
     {
     	System.out.println("Bar is called!!! wooo");
+    	if(i > 0)
+    		System.out.println("i>0");
     }
     public static int i;
     
