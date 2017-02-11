7f25104b94a147f981989b04108b2ee3ec5813a7
Jonathan Bell <jbell@cs.columbia.edu>
2017-02-11 11:21:21 -0500

    Add anon inner

--- src/main/java/edu/gmu/swe/diffcov/test/OtherApp.java
+++ src/main/java/edu/gmu/swe/diffcov/test/OtherApp.java
@@ -2,6 +2,13 @@ package edu.gmu.swe.diffcov.test;
 
 public class OtherApp {
 	public void foo(){
-		
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 }
